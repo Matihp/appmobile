52222e74c13c34f0bc6999e8e8002d3e10c03dcd
matias1364 <matiasleoncontreras@gmail.com>
2023-06-28 21:04:27 -0300

    config

---
+++ src/main/java/org/appandroidmc/entity/Categoria.java
@@ -0,0 +1,48 @@
+package org.appandroidmc.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 100)
+    private String nombre;
+    @Column
+    private Boolean vigencia;
+    @OneToOne
+    private DocumentoAlmacenado foto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(Boolean vigencia) {
+        this.vigencia = vigencia;
+    }
+
+    public DocumentoAlmacenado getFoto() {
+        return foto;
+    }
+
+    public void setFoto(DocumentoAlmacenado foto) {
+        this.foto = foto;
+    }
+}
