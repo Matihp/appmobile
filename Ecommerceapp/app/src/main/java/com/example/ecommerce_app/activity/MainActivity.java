6a328a0bf90b1a9e61eecc1715984f6c7209678b
matias1364 <matiasleoncontreras@gmail.com>
2023-07-18 20:07:08 -0300

    add sidebar

--- Ecommerceapp/app/src/main/java/com/example/ecommerce_app/activity/MainActivity.java
+++ Ecommerceapp/app/src/main/java/com/example/ecommerce_app/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.ecommerce_app;
+package com.example.ecommerce_app.activity;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.ecommerce_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
